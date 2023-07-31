63e2835ee65562928c5bb7e7f415fe5d86e42eef
svartse <killbillbeatrixx@gmail.com>
2023-07-31 21:32:57 +0300

    set up

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0e0f17",
+        lightPrimary: "#313652",
+        purplePrimary: "#201f3bc0",
+        whitePrimary: "#40495e",
+        secondary: "#4828ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+        purple: "#800080",
+        indigo: "#4B0082",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
